d1c51797b1e69363c584c82716875d71e6643318
yinjiaojiao <jiaojiao.yin@downjoy.com>
2020-12-21 11:53:09 +0800

    测试

---
+++ src/main/java/test/testPrint.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * Description.
+ *
+ * @author Y
+ * @date 2020/12/21 11:47
+ */
+public class testPrint {
+    public static void main(String[] args) {
+        System.out.println("111111");
+    }
+}
